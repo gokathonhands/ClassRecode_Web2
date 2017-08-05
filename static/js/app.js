eb70106c7ef5eb7b518ea520ae443b77fe9656f3
ChoiHanJin <zxa018@korea.ac.kr>
2017-08-06 04:54:09 +0900

    test

--- static/js/app.js
+++ static/js/app.js
@@ -133,97 +133,21 @@ if (navigator.mediaDevices.getUserMedia) {
 } else {
     console.log('getUserMedia not supported on your browser!');
 }
-
-$(document).ready(function() {
-    var courseName = [];
-    $.ajax({
-        type: 'GET',
-        url: 'http://52.198.142.127/api/course/',
-        headers: {
-            Authorization: "Token 2864a9d435b07f43b5c56934dd19edf5552ef3ba"
-        }
-        processData: false,
-        contentType: false
-    }).done(function(data) {
-        var timetableItem = "";
-        var day = "";
-        var dayKo = "";
-        for(var i = 0; i < data.length; i++) {
-            timetableItem = document.createElement("button");
-            data[i].day;
-            switch(data[i].day) {
-                case 0:
-                    day = "mon";
-                    dayKo = "월요일";
-                    break;
-                case 1:
-                    day = "tue";
-                    dayKo = "화요일";
-                    break;
-                case 2:
-                    day = "wed";
-                    dayKo = "수요일";
-                    break;
-                case 3:
-                    day = "thu";
-                    dayKo = "목요일";
-                    break;
-                case 4:
-                    day = "fri";
-                    dayKo = "금요일";
-                    break;
-            }
-            timetableItem.className += day + "-" + data[i].startTime + "-s ";
-            timetableItem.className += day + "-" + data[i].endTime + "-e ";
-            timetableItem.className += "timetable-item ";
-            
-            timetableItem.dataset.courseName = data[i].courseName;
-            timetableItem.dataset.dayKo = dayKo;
-            timetableItem.dataset.startTime = data[i].startTime;
-            timetableItem.dataset.endTime = data[i].endTime;
-            
-            var courseNameElement = document.createElement("p");
-            var courseNametextNode = document.createTextNode(data[i].courseName);
-            courseNameElement.appendChild(courseNametextNode);
-            
-            var profNameElement = document.createElement("p");
-            var profNameTextNode = document.createTextNode(data[i].profName);
-            profNameElement.appendChild(profNameTextNode);
-            
-            timetableItem.appendChild(courseNameElement);
-            timetableItem.appendChild(profNameElement);
-            $('.tbl-wrap').append(timetableItem);
-            
-            if (courseName.indexOf(data[i].courseName) < 0) {
-                courseName.push(data[i].courseName);
-            }
-        }
-/**
- * table item background color
- */
-        var backgroundColor = [
-            "#f08676",
-            "#9d86e0",
-            "#ebc469",
-            "#75ca86",
-            "#a6c96f",
-            "#d397ed",
-            "#78a5e8",
-            "#fbab66",
-            "#bf7000",
-            "#1d447c",
-            "#216900",
-            "#d5b400"
-        ]
-        for (var j = 0; j < courseName.length; j++) {
-            for (var i = 0; i < $(".timetable-item").length; i++) {
-                if ($(".timetable-item")[i].dataset.courseName === courseName[j]) {
-                    $(".timetable-item")[i].style.backgroundColor = backgroundColor[j];
-                }
-            }
-        }
-    })
-});
+var backgroundColor = [
+    "#f08676",
+    "#9d86e0",
+    "#ebc469",
+    "#75ca86",
+    "#a6c96f",
+    "#d397ed",
+    "#78a5e8",
+    "#fbab66",
+    "#bf7000",
+    "#1d447c",
+    "#216900",
+    "#d5b400"
+];
+var courseName = [];
 
 $(".timetable-item").on("click", function(){
     var $this = $(this);
@@ -267,5 +191,78 @@ $(".log-in-btn").on("click", function(){
         localStorage.setItem("token", token);
         $(".user").removeClass("not-user");
         $(".user").addClass("is-user");
+        $.ajax({
+            type: 'GET',
+            url: 'http://52.198.142.127/api/course/',
+            headers: {
+                Authorization: localStorage.getItem("token")
+            },
+            processData: false,
+            contentType: false
+        }).done(function(data) {
+            var timetableItem = "";
+            var day = "";
+            var dayKo = "";
+            for(var i = 0; i < data.length; i++) {
+                timetableItem = document.createElement("button");
+                data[i].day;
+                switch(data[i].day) {
+                    case 0:
+                        day = "mon";
+                        dayKo = "월요일";
+                        break;
+                    case 1:
+                        day = "tue";
+                        dayKo = "화요일";
+                        break;
+                    case 2:
+                        day = "wed";
+                        dayKo = "수요일";
+                        break;
+                    case 3:
+                        day = "thu";
+                        dayKo = "목요일";
+                        break;
+                    case 4:
+                        day = "fri";
+                        dayKo = "금요일";
+                        break;
+                }
+                timetableItem.className += day + "-" + data[i].startTime + "-s ";
+                timetableItem.className += day + "-" + data[i].endTime + "-e ";
+                timetableItem.className += "timetable-item ";
+                
+                timetableItem.dataset.courseName = data[i].courseName;
+                timetableItem.dataset.dayKo = dayKo;
+                timetableItem.dataset.startTime = data[i].startTime;
+                timetableItem.dataset.endTime = data[i].endTime;
+                
+                var courseNameElement = document.createElement("p");
+                var courseNametextNode = document.createTextNode(data[i].courseName);
+                courseNameElement.appendChild(courseNametextNode);
+                
+                var profNameElement = document.createElement("p");
+                var profNameTextNode = document.createTextNode(data[i].profName);
+                profNameElement.appendChild(profNameTextNode);
+                
+                timetableItem.appendChild(courseNameElement);
+                timetableItem.appendChild(profNameElement);
+                $('.tbl-wrap').append(timetableItem);
+                
+                if (courseName.indexOf(data[i].courseName) < 0) {
+                    courseName.push(data[i].courseName);
+                }
+            }
+/**
+ * table item background color
+ */
+            for (var j = 0; j < courseName.length; j++) {
+                for (var i = 0; i < $(".timetable-item").length; i++) {
+                    if ($(".timetable-item")[i].dataset.courseName === courseName[j]) {
+                        $(".timetable-item")[i].style.backgroundColor = backgroundColor[j];
+                    }
+                }
+            }
+        })
     })
 })
